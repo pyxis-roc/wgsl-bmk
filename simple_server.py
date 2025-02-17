8420dac9a1f933bd122ddd133d4c36cd579d7177
Benjamin Valpey <36710691+bavalpey@users.noreply.github.com>
2025-02-17 08:45:45 -0600

    Add dequantize

--- simple_server.py
+++ simple_server.py
@@ -8,6 +8,7 @@ __author__ = "Benjamin Valpey"
 __license__ = "MIT"
 import http.server
 import socketserver
+import signal
 
 class CustomHandler(http.server.SimpleHTTPRequestHandler):
     def __init__(self, *args, **kwargs):
@@ -31,9 +32,21 @@ Handler.extensions_map = {
     "": "application/octet-stream",  # Default
 }
 
+global httpd
+def signal_handler(sig, frame):
+    print('Interrupt received, shutting down the server...')
+    if httpd is not None:
+        httpd.server_close()
+        import sys
+        sys.exit(0)
+
 
 if __name__ == '__main__':
+    signal.signal(signal.SIGINT, signal_handler)
     with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
         httpd.allow_reuse_address = True
+        httpd.allow_reuse_port = True
         print("serving at port", PORT)
         httpd.serve_forever()
+
+
