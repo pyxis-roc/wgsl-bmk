d7f8b6a1aedf2f91ecbbfcd3c19c2ffa939e33aa
Benjamin Valpey <36710691+bavalpey@users.noreply.github.com>
2025-03-04 23:46:40 -0600

    Fix broken navbar links for real this time

--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,30 @@
 import { type UserConfig, defineConfig } from "vite";
 
-/** @type {import('vite').UserConfig} */
-export default defineConfig({
-	root: "./src",
-	publicDir: "../public",
-	base: "/wgsl-bmk",
-	build: {
-		outDir: "../dist",
-		emptyOutDir: true,
-		target: "es2022",
-		minify: false,
-		rollupOptions: {
-			input: {
-				index: "/index.html",
-				dequantize: "/benchmarks/dequantize.html",
-				matmul: "/benchmarks/matmul.html",
+export default defineConfig(({ mode }): UserConfig => {
+	return {
+		root: "./src",
+		publicDir: "../public",
+		base: mode === "production" ? "/wgsl-bmk" : undefined,
+		envDir: "../",
+		build: {
+			outDir: "../dist",
+			emptyOutDir: true,
+			target: "es2022",
+			minify: false,
+			rollupOptions: {
+				input: {
+					index: "/index.html",
+					dequantize: "/benchmarks/dequantize.html",
+					matmul: "/benchmarks/matmul.html",
+				},
 			},
 		},
-	},
-	resolve: {
-		alias: {
-			"#classes": "/scripts/classes",
-			"#scripts": "/scripts",
-			"#shaders": "/shaders",
+		resolve: {
+			alias: {
+				"#classes": "/scripts/classes",
+				"#scripts": "/scripts",
+				"#shaders": "/shaders",
+			},
 		},
-	},
-}) satisfies UserConfig;
+	};
+});
